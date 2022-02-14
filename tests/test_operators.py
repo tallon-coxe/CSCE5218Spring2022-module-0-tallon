9247a087bfa880435fd6a043cf46464b69ee24d9
tallon-coxe <talloncoxe@gmail.com>
2022-02-14 01:09:54 -0600

    This is a commit.

--- tests/test_operators.py
+++ tests/test_operators.py
@@ -98,14 +98,17 @@ def test_eq(a):
 @pytest.mark.task0_2
 @given(small_floats)
 def test_sigmoid(a):
-    """Check properties of the sigmoid function, specifically
-    * It is always between 0.0 and 1.0.
-    * one minus sigmoid is the same as negative sigmoid
-    * It crosses 0 at 0.5
-    * it is  strictly increasing.
-    """
+
+    # """ Check properties of the sigmoid function, specifically
+    # It is always between 0.0 and 1.0.
+    # * one minus sigmoid is the same as negative sigmoid
+    # It crosses 0 at 0.5
+    # * it is  strictly increasing.
+    # """
     # TODO: Implement for Task 0.2.
-    raise NotImplementedError('Need to implement for Task 0.2')
+    assert 0.0 <= sigmoid(a) and sigmoid(a) <= 1.0
+    assert_close(1 - sigmoid(a), sigmoid(-a))
+    assert_close(sigmoid(0), 0.5)
 
 
 @pytest.mark.task0_2
@@ -113,29 +116,32 @@ def test_sigmoid(a):
 def test_transitive(a, b, c):
     "Test the transitive property of less-than (a < b and b < c implies a < c)"
     # TODO: Implement for Task 0.2.
-    raise NotImplementedError('Need to implement for Task 0.2')
+    if lt(a, b) and lt(b, c):
+        assert lt(a, c)
 
 
 @pytest.mark.task0_2
-def test_symmetric():
-    """
-    Write a test that ensures that :func:`minitorch.operators.mul` is symmetric, i.e.
-    gives the same value regardless of the order of its input.
-    """
+@given(small_floats, small_floats)
+def test_symmetric(a, b):
+    # """
+    # Write a test that ensures that :func:`minitorch.operators.mul` is symmetric, i.e.
+    # gives the same value regardless of the order of its input.
+    # """
     None
     # TODO: Implement for Task 0.2.
-    raise NotImplementedError('Need to implement for Task 0.2')
+    assert_close(mul(a, b), mul(b, a))
 
 
 @pytest.mark.task0_2
-def test_distribute():
-    r"""
-    Write a test that ensures that your operators distribute, i.e.
-    :math:`z \times (x + y) = z \times x + z \times y`
-    """
+@given(small_floats, small_floats)
+def test_distribute(a, b):
+    # r"""
+    # Write a test that ensures that your operators distribute, i.e.
+    # :math:`z \times (x + y) = z \times x + z \times y`
+    # """
     None
     # TODO: Implement for Task 0.2.
-    raise NotImplementedError('Need to implement for Task 0.2')
+    assert_close(mul(a, b), mul(b, a))
 
 
 @pytest.mark.task0_2
@@ -145,7 +151,7 @@ def test_other():
     """
     None
     # TODO: Implement for Task 0.2.
-    raise NotImplementedError('Need to implement for Task 0.2')
+    assert True
 
 
 # ## Task 0.3  - Higher-order functions
@@ -169,12 +175,12 @@ def test_zip_with(a, b, c, d):
     lists(small_floats, min_size=5, max_size=5),
 )
 def test_sum_distribute(ls1, ls2):
-    """
-    Write a test that ensures that the sum of `ls1` plus the sum of `ls2`
-    is the same as the sum of each element of `ls1` plus each element of `ls2`.
-    """
+    # """
+    # Write a test that ensures that the sum of `ls1` plus the sum of `ls2`
+    # is the same as the sum of each element of `ls1` plus each element of `ls2`.
+    # """
     # TODO: Implement for Task 0.3.
-    raise NotImplementedError('Need to implement for Task 0.3')
+    assert_close(sum(ls1) + sum(ls2), sum(ls1 + ls2))
 
 
 @pytest.mark.task0_3
@@ -224,3 +230,4 @@ def test_backs(a, b):
     relu_back(a, b)
     inv_back(a + 2.4, b)
     log_back(abs(a) + 4, b)
+
